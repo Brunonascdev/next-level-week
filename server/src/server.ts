cf135988e1cd7d164008895b26abc930f42a59e5
Bruno Nascimento <brunonascimentodev@gmail.com>
2020-06-01 10:10:44 -0300

    Adding gitignore to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json([
-    { id: "1", name: "Diego" },
-    { id: "2", name: "Bruno" },
-  ]);
+  return res.send("Hello World!");
 });
 
 app.listen(3333);
